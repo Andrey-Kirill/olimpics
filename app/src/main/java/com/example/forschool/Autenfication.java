7b1080cb219e6de78b91a7008e2e2f14af411956
Alexandr <Weroset64@gmail.com>
2020-10-19 12:43:36 +0300

    firebase

--- app/src/main/java/com/example/forschool/Autenfication.java
+++ app/src/main/java/com/example/forschool/Autenfication.java
@@ -15,12 +15,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Autenfication extends AppCompatActivity {
     EditText email;
     EditText passwod;
     FirebaseAuth autenfication;
-
+    DatabaseReference mDataBase;
+    String USER_KEY = "User";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,7 @@ public class Autenfication extends AppCompatActivity {
         email = findViewById(R.id.editTextTextEmailAddress);
         passwod = findViewById(R.id.editTextTextPassword);
         autenfication = FirebaseAuth.getInstance();
+        mDataBase = FirebaseDatabase.getInstance().getReference(USER_KEY);
     }
 
 
@@ -53,8 +57,40 @@ public class Autenfication extends AppCompatActivity {
             Toast.makeText(this,"Please enter your data",Toast.LENGTH_SHORT).show();
         }
     }
-    public void signin(View v){
+    public void signin(final View v) {
+        if ((email.getText().toString().isEmpty() && passwod.getText().toString().isEmpty()) == false) {
+            autenfication.signInWithEmailAndPassword(email.getText().toString(), passwod.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful() == true) {
+                        FirebaseUser user = autenfication.getCurrentUser();
+                        Toast.makeText(getApplicationContext(), "You signed as " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(v.getContext(), MainActivity.class);
+                        startActivity(i);
+                    } else {
+                        Toast.makeText(v.getContext(), "You can not sign in", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "Please enter your data", Toast.LENGTH_SHORT).show();
+        }
 
     }
+/*
+    public void push(View v){
+
+        String id = mDataBase.getKey();
+        String txt = email.getText().toString();
+        User user = new User(id,txt);
+
+        mDataBase.push().setValue(user);
+        Toast.makeText(v.getContext(),"ok",Toast.LENGTH_SHORT).show();
+
+   }
+
+
+ */
+
 }
 
